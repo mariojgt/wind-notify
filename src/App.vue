729ec7c8e3cd1be5b4759ee0b65172fe6de25837
Mario Jose Goes Tarosso <mariojgt2@gmail.com>
2022-07-30 15:38:45 +0100

    updates

--- src/App.vue
+++ src/App.vue
@@ -18,9 +18,6 @@ import navbar from "./components/global/navbar.vue";
 import block01 from "./components/Block01.vue";
 import genericBlock from "./components/global/GenericBlock.vue";
 
-// Examples
-import inputExample from "./components/examples/inputs.vue";
-
 // Update the page tittle
 document.title = "Mario Tarosso | Projects";
 </script>
